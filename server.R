ccf436aaf8cdbe2d7ea6b6aa9aae7177096b2f30
John Bennett <38082722+JohnBennett66@users.noreply.github.com>
2020-06-27 12:30:37 -0700

    quick save

--- server.R
+++ server.R
@@ -9,39 +9,39 @@ library(dplyr)
 library(data.table)
 library(lubridate)
 library(usmap)
+library(shinythemes)
 ###
 
 ###  CAPTURE AND TRANSFORM DATA :: JH DATASET  ####
 # data.world covid-19 datasource
-us.date <- fread("https://query.data.world/s/cgpumxcw4ajvqt6334dwjhay6uhact",
+us.state <- fread("https://query.data.world/s/cgpumxcw4ajvqt6334dwjhay6uhact",
                  check.names = TRUE,
                  drop = c("cumulative_cases_per_100_000",  "cumulative_deaths_per_100_000",
                           "new_cases_7_day_rolling_avg",  "new_deaths_7_day_rolling_avg",
                           "new_deaths_per_100_000", "new_cases_per_100_000"))
-
-
 ### DATA TRANSFORMATIONS  ####
 # friendly names
-colnames(us.date) <- c("state", "date", "pop",
+colnames(us.state) <- c("state", "date", "pop",
                        "cum_cases", "cum_deaths",
-                       "Daily Cases", "Daily Deaths")
+                       "daily_cases", "daily_deaths")
 
 # roll up the states for US daily totals
-us.date <- us.date[
+us.date <- us.state[
   ,.(pop = sum(pop),
      cum_cases = sum(cum_cases), cum_deaths = sum(cum_deaths),
-     `Daily Cases` = sum(`Daily Cases`), `Daily Deaths` = sum(`Daily Deaths`) )
+     daily_cases = sum(daily_cases), daily_deaths = sum(daily_deaths) )
   , by = .(date)
   ]
-
 # NA = 0
+setnafill(us.state, fill = 0, cols = 6:7)
 setnafill(us.date, fill = 0, cols = 5:6)
-
 # vars
 start.date = ymd(20200229)
 end.date = ymd(us.date[,max(date)])
 d <- end.date - start.date
-
+# adjust data types
+us.state[, date := ymd(date)]
+us.date[, date := ymd(date)]
 # transformations
 us.date[,cases_per_capita := (cum_cases/pop)]
 us.date[,deaths_per_capita := (cum_deaths/pop)]
@@ -49,26 +49,28 @@ us.date[,cases_per_mill := (cum_cases/(pop/1000000))]
 us.date[,deaths_per_mill := (cum_deaths/(pop/1000000))]
 us.date[,cases_per_10mill := (cum_cases/(pop/10000000))]
 us.date[,deaths_per_10mill := (cum_deaths/(pop/10000000))]
-
 # moving averages
 # basic :: 5 day
-us.date[ , five_day_cases := frollmean(us.date[,`Daily Cases`], 5) ]
-us.date[ , five_day_deaths := frollmean(us.date[,`Daily Deaths`], 5) ]
+us.date[ , five_day_cases := frollmean(us.date[,daily_cases], 5) ]
+us.date[ , five_day_deaths := frollmean(us.date[,daily_deaths], 5) ]
 # extended :: 14 day
-us.date[ , twowk_day_cases := frollmean(us.date[,`Daily Cases`], 14) ]
-us.date[ , twowk_day_deaths := frollmean(us.date[,`Daily Deaths`], 14) ]
-
+us.date[ , twowk_day_cases := frollmean(us.date[,daily_cases], 14) ]
+us.date[ , twowk_day_deaths := frollmean(us.date[,daily_deaths], 14) ]
+# week over week change
+us.state[,cum_cases_lstwk := shift(cum_cases,7), by = state]
+us.state[,pct_chng_lstwk := ((cum_cases - cum_cases_lstwk)/cum_cases_lstwk)*100]
+us.state[,cum_deaths_lstwk := shift(cum_deaths,7), by = state]
+us.state[,pct_deaths_lstwk := ((cum_deaths - cum_deaths_lstwk)/cum_deaths_lstwk)*100]
 # NA = 0
+setnafill(us.state, fill = 0, cols = 8:11)
 setnafill(us.date, fill = 0, cols = 13:16)
-
-# adjust data types
-us.date[, date := ymd(date)]
-
 # variables
-avg.c <- mean(us.date[,`Daily Cases`]) 
-avg.d <- mean(us.date[,`Daily Deaths`])
+avg.c <- mean(us.date[,daily_cases]) 
+avg.d <- mean(us.date[,daily_deaths])
 tot.c <- max(us.date[,cum_cases])
 tot.d <- max(us.date[,cum_deaths])
+
+
 
 
 ###  MAIN FUNCTION  ####
@@ -80,10 +82,10 @@ function(input, output) {
  #   
  # } )
  
-output$plot <- renderPlot( {
+output$plot_dus <- renderPlot( {
   
-  if(input$type == "Daily Cases") ggplot(data = us.date) + 
-      geom_col(aes(x = date, y = `Daily Cases`), fill = "darkorange3") +
+  if(input$daily == 'cases') ggplot(data = us.date) + 
+      geom_col(aes(x = date, y = daily_cases), fill = "darkorange3") +
     geom_line(aes(x = date, y = five_day_cases), colour = 'blue', size = 1) +
     geom_hline(aes(yintercept = avg.c), colour = 'grey1', size = 1) +
     scale_x_date(limits = c(start.date, end.date + 1)) +
@@ -95,8 +97,8 @@ output$plot <- renderPlot( {
                           sep = "\n"),
          y = "Cumulative Deaths",
          x = "2020") 
-  else ggplot(data = us.date) +
-    geom_col(aes(x = date, y = `Daily Deaths`), fill = "darkorange3") +
+  else {ggplot(data = us.date) +
+    geom_col(aes(x = date, y = daily_deaths), fill = "darkorange3") +
     geom_line(aes(x = date, y = five_day_deaths), colour = 'blue', size = 1) +
     geom_hline(aes(yintercept = avg.d), colour = 'grey1', size = 1) +
     scale_x_date(limits = c(start.date, end.date + 1)) +
@@ -107,13 +109,22 @@ output$plot <- renderPlot( {
                           "Five Day Moving Average :: Blue Line",
                           sep = "\n"),
          y = "Cumulative Deaths",
-         x = "2020")
+         x = "2020")}
   
   
   
   
 } )
   
-output$table <- renderTable(us.date[ , .(date,`Daily Cases`)])
+output$plot_st <- renderPlot( {    ## cumulative and scope
+  
+  plot_usmap("states", data = us.state[date == max(date)], values = "cum_cases", color = "blue4") + 
+    scale_fill_continuous(low = "lightblue1", high = "blue4", name = "Total Cases", label = scales::comma) + 
+    theme(panel.background = element_rect(colour = "black", fill = "lightyellow")) + 
+    labs(title = "Total Cases by State",
+         subtitle = paste0("For ",us.date[,max(date)])
+    )
+  
+  })
 
 }
--- ui.R
+++ ui.R
@@ -10,33 +10,61 @@ library(dplyr)
 library(data.table)
 library(lubridate)
 library(usmap)
+library(shinythemes)
 ###
 
 
 
 
 ###  PAGE LAYOUT :: MAIN & SIDEBAR :: SINGLE PLOT  ####
-bootstrapPage(
+fluidPage(
 
-  titlePanel("COVID-19 DATA TRACKING APPLICATION", windowTitle = "CovidTracker"),
-  
-  
-  sidebarLayout( 
-  
-    sidebarPanel(
-     varSelectInput('type', "Select a data type", us.date[,5:6]),
-     width = 3
-    ), #sidebar
-  
-    mainPanel(
+  navbarPage("COVID-19 DATA TRACKING", theme = shinytheme("spacelab"), 
+             
+             tabPanel("Daily US", fluid = TRUE, icon =icon("flag-usa"), 
+                      
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          em("Which Data?", style="font-size:15px"),
+                          shinythemes::themeSelector(),
+                          selectInput('daily', p("Cases or Deaths?", style="font-size:13px"), 
+                                      c("Daily Cases" = "cases", "Daily Deaths" = "deaths"))
+                          #    width = 3
+                          
+                        ),
+                        mainPanel(
+                          
+                          plotOutput('plot_dus')
+                          
+                        ),
+                      ),
+                     ), #tab-daily-us
     
-    textOutput('text'),
+             tabPanel("State Comparisons", fluid = TRUE, icon =icon("flag-usa"), 
+                      
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          selectInput('cumulative', p("Cases or Deaths?", style="font-size:13px"), 
+                                      c("Cumulative Cases" = "cases", "Cumulative Deaths" = "deaths")),
+                          selectInput("scope", p("Total this Week or Percent Change from Last Week"), 
+                                      c("Total" = "total", "Percent Change" = "change"))
+                          
+                        ),
+                        mainPanel(
+                          
+                          plotOutput('plot_st')
+                          
+                        ),
+                      ),
+             ) #tab-state-comp
     
-    plotOutput('plot')
     
-    ), #main
- 
-  position = "right") #sidebarlayout
-
+    
+  ), # navbarpage
+  
+  
+  
 ) #fluidpage
 
