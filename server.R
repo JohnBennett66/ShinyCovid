2a75868f963c946128ee6da05c29e91b4e5ce69f
John Bennett <38082722+JohnBennett66@users.noreply.github.com>
2020-06-26 21:57:21 -0700

    mvp -- save 26 June 2020 set up github repo

--- server.R
+++ server.R
@@ -30,7 +30,20 @@ function(input, output) {
  
 output$plot <- renderPlot( {
   
-  if(input$type == "Daily Cases")  ggplot(us.date) + geom_col(aes(date,`Daily Cases`))
+  if(input$type == "Daily Cases")  ggplot(us.date) + geom_col(aes(date,`Daily Cases`)) 
+  else ggplot(data = us.date) +
+    geom_col(aes(x = date, y = `Daily Deaths`), fill = "darkorange3") +
+    geom_line(aes(x = date, y = five_day_deaths), colour = 'blue', size = 1) +
+    geom_hline(aes(yintercept = average), colour = 'grey1', size = 1) +
+    scale_x_date(limits = c(start.date, end.date + 1)) +
+    labs(title = paste0("Daily Deaths Covid-19 in US by Date — 29 February to current (",end.date,")"),
+         subtitle = paste(paste0("Total US ",input$type," :: ", total ),
+                          paste0(input$type," per million Americans :: ", as.integer(total / 327) ),
+                          paste0("Days since 29 February 2020 :: ", d[[1]]),
+                          "Five Day Moving Average :: Blue Line",
+                          sep = "\n"),
+         y = "Cumulative Deaths",
+         x = "2020")
   
   
   
