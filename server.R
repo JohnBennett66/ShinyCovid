1d552a656a80d52dd27bd4815b3880864f915d5e
John Bennett <38082722+JohnBennett66@users.noreply.github.com>
2020-06-29 15:11:21 -0700

    First three tabs v1 complete Added About tab 29 June 2020 jmb 8^>

--- server.R
+++ server.R
@@ -62,34 +62,56 @@ us.state[,cum_cases_lstwk := shift(cum_cases,7), by = state]
 us.state[,pct_chng_lstwk := ((cum_cases - cum_cases_lstwk)/cum_cases_lstwk)*100]
 us.state[,cum_deaths_lstwk := shift(cum_deaths,7), by = state]
 us.state[,pct_deaths_lstwk := ((cum_deaths - cum_deaths_lstwk)/cum_deaths_lstwk)*100]
+# moving averages - states
+# basic :: 5 day
+us.state[ , five_day_cases := frollmean(us.state[,daily_cases], 5) ]
+us.state[ , five_day_deaths := frollmean(us.state[,daily_deaths], 5) ]
+# extended :: 14 day
+us.state[ , twowk_day_cases := frollmean(us.state[,daily_cases], 14) ]
+us.state[ , twowk_day_deaths := frollmean(us.state[,daily_deaths], 14) ]
 # NA = 0
-setnafill(us.state, fill = 0, cols = 8:11)
+setnafill(us.state, fill = 0, cols = 8:15)
 setnafill(us.date, fill = 0, cols = 13:16)
 # variables
 avg.c <- mean(us.date[,daily_cases]) 
 avg.d <- mean(us.date[,daily_deaths])
 tot.c <- max(us.date[,cum_cases])
 tot.d <- max(us.date[,cum_deaths])
-
-
+# selections
+us.state.chng <- us.state[date == max(date), .(state ,pct_chng_lstwk)]
+setorder(us.state.chng, -pct_chng_lstwk)
+mn <- us.state.chng[,mean(pct_chng_lstwk)]
+sd <- sd(us.state.chng[,pct_chng_lstwk])
+sd.1 <- mn + sd
+sd.2 <- mn + (sd*2)
+max <- us.state.chng[,max(pct_chng_lstwk)]
+cnt.mn <- count(us.state.chng[pct_chng_lstwk >= mn])
+cnt.1 <- count(us.state.chng[pct_chng_lstwk >= sd.1])
+ifelse(cnt.mn <= 15, us.state.rise <- us.state.chng[pct_chng_lstwk >= mn, state],  
+  ifelse(cnt.1 <= 15, us.state.rise <- us.state.chng[pct_chng_lstwk >= sd.1, state],
+         us.state.rise <- us.state.chng[pct_chng_lstwk >= sd.2, state]
+  )
+)
 
 
 ###  MAIN FUNCTION  ####
 function(input, output) {
   
- # output$plot <- renderPlot( {
- #   ggplot(us.date) + 
- #     geom_col(aes(date,input$type))
- #   
- # } )
+output$st.lst <- renderText({
+  paste(toString(us.state.rise),"\n")
+})
  
+output$test <- renderText( { 
+  paste(toString(us.state[state == "Washington", input$daily]))
+  })
 
-  
+##  US OVERALL DAILY LINE CHART :: CASES OR DEATHS 
 output$plot_dus <- renderPlot( {
   
   if(input$daily == 'cases') ggplot(data = us.date) + 
       geom_col(aes(x = date, y = daily_cases), fill = "darkorange3") +
     geom_line(aes(x = date, y = five_day_cases), colour = 'blue', size = 1) +
+    geom_line(aes(x = date, y = twowk_day_cases), colour = 'dodgerblue', size = 1) +
     geom_hline(aes(yintercept = avg.c), colour = 'grey1', size = 1) +
     scale_x_date(limits = c(start.date, end.date + 1)) +
     labs(title = paste0("Daily Cases Covid-19 in US by Date — 29 February to current (",end.date,")"),
@@ -97,12 +119,14 @@ output$plot_dus <- renderPlot( {
                           paste0("Cases per million Americans :: ", format(as.integer(tot.c / 327), big.mark = ",") ),
                           paste0("Days since 29 February 2020 :: ", d[[1]]),
                           "Five Day Moving Average :: Blue Line",
+                          "14 Day Moving Average :: Light Blue Line",
                           sep = "\n"),
          y = "Cumulative Deaths",
          x = "2020") 
   else {ggplot(data = us.date) +
     geom_col(aes(x = date, y = daily_deaths), fill = "darkorange3") +
     geom_line(aes(x = date, y = five_day_deaths), colour = 'blue', size = 1) +
+    geom_line(aes(x = date, y = twowk_day_deaths), colour = 'dodgerblue', size = 1) +
     geom_hline(aes(yintercept = avg.d), colour = 'grey1', size = 1) +
     scale_x_date(limits = c(start.date, end.date + 1)) +
     labs(title = paste0("Daily Deaths Covid-19 in US by Date — 29 February to current (",end.date,")"),
@@ -110,6 +134,7 @@ output$plot_dus <- renderPlot( {
                           paste0("Deaths per million Americans :: ", format(as.integer(tot.d / 327), big.mark = ",") ),
                           paste0("Days since 29 February 2020 :: ", d[[1]]),
                           "Five Day Moving Average :: Blue Line",
+                          "14 Day Moving Average :: Light Blue Line",
                           sep = "\n"),
          y = "Cumulative Deaths",
          x = "2020")}
@@ -117,6 +142,7 @@ output$plot_dus <- renderPlot( {
 } )
   
 
+##  US MAP CHART :: CUMULATIVE RANKING :: CASESE OR DEATHS :: DISPLAYED WITH BELOW
 output$plot_st <- renderPlot( {    ## cumulative and scope cases/deaths total/change
   
   if(input$cumulative == "cases")  
@@ -133,9 +159,10 @@ output$plot_st <- renderPlot( {    ## cumulative and scope cases/deaths total/ch
            subtitle = paste0("For ",us.date[,max(date)]))
   
   
-  })
+  }
+)
 
-
+##  US MAP CHART :: PREVIOUS WEEK CHANGE RANKING :: DISPLAYED WITH ABOVE 
 output$plot_chg <- renderPlot( {    ## cumulative and scope cases/deaths total/change
   
   if(input$cumulative == "cases")
@@ -145,13 +172,64 @@ output$plot_chg <- renderPlot( {    ## cumulative and scope cases/deaths total/c
       labs(title = "Percent Change for each State on Total Cases — Compared to Last Week",
            subtitle = paste0("For ",us.date[,max(date)]) )
   else 
-    plot_usmap("states", data = us.state[date == max(date)], values = "cum_deaths", color = "firebrick") +
+    plot_usmap("states", data = us.state[date == max(date)], values = "pct_deaths_lstwk", color = "firebrick") +
       scale_fill_continuous(low = "lightpink", high = "orangered", name = "Percent Change", label = scales::comma) +
       theme(panel.background = element_rect(colour = "black", fill = "lightyellow")) +
-      labs(title = "Percent Change for each State on Total Cases — Compared to Last Week",
+      labs(title = "Percent Change for each State on Total Deaths — Compared to Last Week",
            subtitle = paste0("For ",us.date[,max(date)]))
   
-})
+},
+width = 'auto',
+height = 'auto'
+)
+
+
+##  SINGLE STATE DAILY LINE CHART :: CASES OR DEATHS 
+output$plot_dss_cases <- renderPlot( {
+  ggplot(data = us.state[state == input$state]) +
+    geom_col(aes(x = date, y = daily_cases), fill = "darkorange3") +
+    geom_hline(aes(yintercept = us.state[state == input$state, mean(daily_cases)]), colour = 'grey1', size = 1) +
+    scale_x_date(limits = c(start.date, end.date + 1)) +
+    labs(title = paste0("Daily Cases Covid-19 in ",input$state," by Date — 29 February to current (",end.date,")"),
+         subtitle = paste(paste0("For ",input$state," ::"),
+                          paste0("Total Cases :: ", format(us.state[state == input$state,max(cum_cases)], big.mark = ",") ),
+                          paste0("Total Deaths :: ", format(us.state[state == input$state,max(cum_deaths)], big.mark = ",") ),
+                          paste0("Days since 29 February 2020 :: ", d[[1]]),
+                          "Average :: Grey Line",
+                          sep = "\n"),
+         y = "Daily Cases",
+         x = "2020")
+  } ,
+width = 'auto',
+height = 'auto'
+
+)
+    
+    
+output$plot_dss_death <- renderPlot( {    
+    
+ggplot(data = us.state[state == input$state]) +
+    geom_col(aes(x = date, y = daily_deaths), fill = "darkorange3") +
+    geom_hline(aes(yintercept = us.state[state == input$state, mean(daily_deaths)]), colour = 'grey1', size = 1) +
+    scale_x_date(limits = c(start.date, end.date + 1)) +
+    labs(title = paste0("Daily Deaths Covid-19 in ",input$state," by Date — 29 February to current (",end.date,")"),
+         subtitle = paste(paste0("For ",input$state," ::"),
+                          paste0("Total Cases :: ", format(us.state[state == input$state,max(cum_cases)], big.mark = ",") ),
+                          paste0("Total Deaths :: ", format(us.state[state == input$state,max(cum_deaths)], big.mark = ",") ),
+                          paste0("Days since 29 February 2020 :: ", d[[1]]),
+                          "Average :: Grey Line",
+                          sep = "\n"),
+         y = "Daily Deaths",
+         x = "2020")
+} ,
+width = 'auto',
+height = 'auto'
+)
+
+
+
+
+
 
 } # function
 
--- ui.R
+++ ui.R
@@ -27,11 +27,10 @@ fluidPage(
                       sidebarLayout(
                         sidebarPanel(
                           
-                          em("Which Data?", style="font-size:15px"),
                           # shinythemes::themeSelector(),
-                          selectInput('daily', p("Cases or Deaths?", style="font-size:13px"), 
-                                      c("Daily Cases" = "cases", "Daily Deaths" = "deaths"))
-                          #    width = 3
+                          selectInput('daily', p("Which Data? Cases or Deaths", style="font-size:13px"), 
+                                      c("Daily Cases" = "cases", "Daily Deaths" = "deaths")),
+                          width = 2
                           
                         ),
                         mainPanel(
@@ -42,15 +41,14 @@ fluidPage(
                       ),
                      ), #tab-daily-us
     
-             tabPanel("State Comparisons", fluid = TRUE, icon =icon("flag-usa"), 
+             tabPanel("State Comparisons", fluid = TRUE, 
                       
                       sidebarLayout(
                         sidebarPanel(
                           
                           selectInput('cumulative', p("Cases or Deaths?", style="font-size:13px"), 
-                                      c("Cumulative Cases" = "cases", "Cumulative Deaths" = "deaths"))
-                          
-                          
+                                      c("Cumulative Cases" = "cases", "Cumulative Deaths" = "deaths")),
+                          width = 2
                         ),
                         mainPanel(
                           
@@ -59,9 +57,69 @@ fluidPage(
                           
                         ),
                       ),
-             ) #tab-state-comp
+             ), #tab-state-comp
     
-    
+             tabPanel("Daily Single State", fluid = TRUE, 
+                      
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          selectInput('state', p("Which State", style="font-size:13px"), 
+                                      us.state[,unique(state)], selected = "Alabama"),
+                          
+                          p(),
+                          p("These states had higher than average change last week ::",
+                             style="font-size:15px"),
+                          p(),
+                          textOutput('st.lst'),
+                          p(),
+                          em("Listed in descending order.",
+                             style="font-size:11px"),
+                          width = 2
+                        ),
+                       
+                        mainPanel(
+                         
+                          plotOutput('plot_dss_cases'),
+                          plotOutput('plot_dss_death')
+                          
+                        ),
+                      ),
+             ), #tab-single-state
+             
+             
+             
+             
+             tabPanel("About", fluid = TRUE,
+                      mainPanel(
+                        h3("About the Data"),
+                        p("This data is provided from John Hopkins."),
+                        p("The data is available from dataworld.com, 
+                          which is where I refresh the data for this app."),
+                        p("The visualizations are mine, not that they are so fancy."),
+                        p("Regarding the app, I had inspiration/assistance from the 
+                          NCAA Swim Team Finder app in the Shiny gallery."),
+                        p("You can see it here, ",
+                        a(href="https://shiny.rstudio.com/gallery/ncaa-swim-team-finder.html",
+                          "NCAA Swim Team Finder Shiny App Example")),
+
+                        
+                        h3("About the Author"),
+                        p("I am John Bennett. I am a data scientest looking to
+                          improve my skills so I built this Shiny app for fun."),
+                        p("If you have suggestions for this app or want to reach
+                          me for any reason, you can find me here."),
+                        p("Github :: JohnBennett66"),
+                        p("also,"),
+                        p("Twitter ::  @john_bennett"),
+                        p("LinkedIn :: https://www.linkedin.com/in/johnbennett/"),
+                        p(),
+                        p("This app can also be reached at ",a(href="bit.ly/jb66ct","bit.ly/jb66ct"))
+
+                      )
+               
+             ) # ABOUT TAB
+             
     
   ), # navbarpage
   
